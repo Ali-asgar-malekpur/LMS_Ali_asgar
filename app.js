5a4fd0272e9b7604cc1ff90e7efd7e09bb5891e4
Ali asgar Malekpur <82698658+Ali-asgar-malekpur@users.noreply.github.com>
2022-05-17 11:13:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,24 +8,20 @@ const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 
 const app = express();
 
-// middleware
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyparser.urlencoded({ extended : true}))
 
-// view engine
 app.set('view engine', 'ejs');
 
 app.use(morgan('tiny'));
 
-// database connection
 const dbURI = 'mongodb+srv://Ali_asgar:Aliasgar_@123@cluster0.0et9u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
-// routes
 app.get('*', checkUser);
 app.get('/', (req, res) => res.render('home'));
 app.get('/Courses', requireAuth, (req, res) => res.render('Courses'));
